49a0a217e562430786bc6618b99a7dd65bea888d
杨少 <samyangcoder@gmail.com>
2017-09-05 18:42:36 +0800

    update

---
+++ src/components/dialogBox.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="message" :class="classObj">{{ message }}</div>
+</template>
+
+<script>
+export default {
+  name: 'dialog-box',
+  props: ['message', 'type'],
+  computed: {
+    classObj () {
+      return {
+        left: this.type === 'left',
+        right: this.type === 'right',
+      }
+    },
+  }
+}
+</script>
+
+<style>
+.message {
+  width: fit-content;
+  width: -webkit-fit-content;
+  width: -moz-fit-content;
+  max-width: 350px;
+  border: 1px solid #D3DCE6;
+  padding: 10px;
+  border-radius: 5px;
+  margin: 10px 0;
+}
+.left {
+  background-color: #F9FAFC;
+  float: left;
+  clear: right;
+}
+.right {
+  background-color: #35BD27;
+  float: right;
+  clear: left;
+}
+</style>
--- src/components/mainPane.vue
+++ src/components/mainPane.vue
@@ -6,13 +6,17 @@
       </el-col>
     </el-row>
     <div class="content">
-      <p v-for="n in 20" :key="n">{{ n }}</p>
+      <dialog-box v-for="n in 1" :key="n" :message="msg" :type="'left'"></dialog-box>
+      <dialog-box v-for="n in 1" :key="n" :message="msg" :type="'right'"></dialog-box>
+      <dialog-box v-for="n in 1" :key="n" :message="msg" :type="'left'"></dialog-box>
+      <dialog-box v-for="n in 1" :key="n" :message="msg" :type="'right'"></dialog-box>
     </div>
+    <div class="placeholder"></div>
     <el-row class="tac input-bar">
-      <el-col :span="16" :offset="6">
+      <el-col :span="16">
         <el-input v-model="input" placeholder="请输入聊天内容"></el-input>
       </el-col>
-      <el-col :span="0">
+      <el-col :span="2">
         <el-button type="primary" class="send-btn">发送</el-button>
       </el-col>
     </el-row>
@@ -20,8 +24,13 @@
 </template>
 
 <script>
+import dialogBox from './dialogBox'
+
 export default {
   name: 'main-pane',
+  components: {
+    dialogBox,
+  },
   computed: {
     memberName () {
       return this.$route.params.name || 'Sam Yang'
@@ -29,7 +38,9 @@ export default {
   },
   data () {
     return {
-      input: ''
+      input: '',
+      msg: '你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊',
+      type: 'right',
     }
   }
 }
@@ -53,19 +64,22 @@ export default {
   font-size: 1.2em;
   margin: 0;
   text-align: center;
-  color: #48576a; 
+  color: #48576a;
 }
 .member-name i {
   cursor: pointer;
 }
 .content {
-  padding-top: 40px;
-  padding-bottom: 50px;
+  padding: 40px 25px 50px;
+}
+.placeholder {
+  width: 100%;
+  height: 36px;
+  float: left;
 }
 .input-bar {
   width: 100%;
   position: fixed;
-  left: 0;
   bottom: 0;
 }
 .send-btn {
